48c0f6464cc074cfbb20add6dc7537a1111f3076
Mike Preston <mwpreston@gmail.com>
2018-10-26 11:25:23 -0400

    creds

--- Rubrik/SLAChangeManagement/MonitorRubrikSLADomains.ps1
+++ Rubrik/SLAChangeManagement/MonitorRubrikSLADomains.ps1
@@ -1,41 +1,37 @@
 ﻿param (
     [Parameter(Mandatory=$true)]
-    [ValidateScript({Test-Path $_})]
-    [String]$EnvironmentFile
+    [ValidateScript({Test-Path $_\config.json})]
+    [String]$ConfigFolder
 )
+
+clear
+
+Import-Module Rubrik
 
 function CheckForCredentials
 {
     #Check For Rubrik Credentials
-    $CredFile = $IdentityPath + $Environment.rubrikCred
+    $CredFile = $ConfigFolder + $Config.rubrikCred
     If (-not (Test-Path -Path $CredFile))
     {
         Write-Host -ForegroundColor Yellow "$CredFile does not exist"
         $null = Read-Host "Press any key to continue and create the appropriate credential files"
         CreateCredentialFile -FilePath $CredFile -Message "Please enter a username and password with access to the Rubrik cluster..."
     }
-
-    #Check for SQL Credentials
-    foreach ($database in $config.databases)
-    {
-        $CredFile = $IdentityPath + $database.TargetDBSQLCredentials
-        If (-not (Test-Path -Path $CredFile))
-        {
-            Write-Host -ForegroundColor Yellow "$CredFile does not exist"
-            $null = Read-Host "Press any key to continue and create the appropriate credential files"
-            CreateCredentialFile -FilePath $CredFile -Message "Please enter a SQL username and password with access to $($database.TargetDBServer).  ***NOTE*** This must be a SQL account - Domain accounts are not supported."
-        }
-    }
-
 }
-
-#function to create xml credentials files if they do not exist
 function CreateCredentialFile ($FilePath, $Message)
 {
     $Credential = Get-Credential -Message $Message
     $Credential | Export-Clixml -Path ($FilePath)
 }
-clear
+#append backslash if not there
+if ($ConfigFolder -notmatch '.+?\\$') { $ConfigFolder += '\' }
 
-Import-Module Rubrik
+#path to config file
+$ConfigFile = $ConfigFolder + "config.json"
+
+#load config into variable
+$script:Config = Get-Content -Path $ConfigFile | ConvertFrom-Json
+
+CheckForCredentials
 
