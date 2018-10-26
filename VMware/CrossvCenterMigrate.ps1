c496b734196bf129c703e1d3a6c6bceb5baa8bc8
Mike Preston <mwpreston@gmail.com>
2018-10-26 10:23:55 -0400

    first commit

---
+++ VMware/CrossvCenterMigrate.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module VMware.vim
+$sourceCreds = Get-Credential
+$targetCreds = Get-Credential
+
+$sourceVC = Connect-VIServer -Server vcsa.rubrik.us -Credential $sourceCreds
+$destinationVC = Connect-VIServer -Server vcsa03.rubrik.us -Credential $targetCreds
+
+$destinationHost = "esxi41.rubrik.us"
+$destinationDatastore = Get-Datastore -Server $destinationVC -Name "Datastore"
+$destinationPortGroup = Get-VirtualPortGroup -Server $destinationVC -Name 'VM_Production'
+
+$vm = Get-VM -Server $sourceVC "MPRESTON-VM2"
+$sourceNetworkAdapter = Get-NetworkAdapter -VM $vm
+
+Move-VM -VM $vm -VMotionPriority High -Destination (Get-VMHost -Server $destinationVC -Name $destinationHost)
+  -NetworkAdapter $networkAdapter -PortGroup $destinationPortGroup -Datastore $destinationDatastore
