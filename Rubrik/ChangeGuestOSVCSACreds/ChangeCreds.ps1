49caf0f1b5c875b0b11784d51ced82329068f821
Mike Preston <mwpreston@gmail.com>
2018-12-17 16:09:35 -0500

    done

--- Rubrik/ChangeGuestOSVCSACreds/ChangeCreds.ps1
+++ Rubrik/ChangeGuestOSVCSACreds/ChangeCreds.ps1
@@ -37,8 +37,48 @@ CheckForCredentials
 
 $Credential = Import-Clixml -Path ($ConfigFolder + $Config.rubrikCred)
 $RubrikCluster = $config.rubrikServer
+
 #Build header
 $auth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Credential.username + ':' + $Credential.GetNetworkCredential().password))
 $header = @{'Authorization' = "Basic $auth" }
+$token = ((Invoke-WebRequest -Headers $header -Method "POST" -Uri "https://$RubrikCluster/api/v1/session").Content | ConvertFrom-Json ).token
+#update header to token for internal API calls
+$header = @{'Authorization' = "Bearer $token"}
+#=======================================
+#Change vCenter Password inside Rubrik
+#=======================================
+$vcentername = "vcsa03.rubrik.us"
+$vcenterusername = "administrator@vsphere.local"
+$vcenterpassword = "SuperSecretPassword"
 
-Invoke-WebRequest -Header $header -Method "POST" -Uri "https://$RubrikCluster/api/v1/session"
+#Get vCenter ID
+$response = Invoke-WebRequest -Method "Get" -Uri "https://$RubrikCluster/api/v1/vmware/vcenter" -Headers $head
+$vcenterid = ((($response.Content | ConvertFrom-Json).Data) | Where-object { $_.name -eq "$vcentername" }).id
+
+$body = @{
+"hostname" = "$vcentername";
+"username" = "$vcenterusername";
+"password" = "$vcenterpassword";
+} | ConvertTo-Json
+$response = Invoke-WebRequest -Method "PUT" -uri "https://$RubrikCluster/api/v1/vmware/vcenter/$vcenterid" -Headers $head -Body $body
+#================================================
+# vCenter Done
+#================================================
+
+#================================================
+#Guest OS Credentials
+#================================================
+$credentialUsername = "mike.preston@rubrik.us"
+$credentialPassword = "NewSecretPassword"
+# get credential ID
+$response = Invoke-WebRequest -Method "Get" -Uri "https://$RubrikCluster/api/internal/vmware/guest_credential" -Headers $head
+$credentialID = ((($response.Content | ConvertFrom-Json).Data) | Where-object { $_.username -eq "$credentialUsername" }).id
+$body = @{
+    "username" = "$credentialUsername";
+    "password" = "$credentialPassword";
+    } | ConvertTo-Json
+$response = Invoke-WebRequest -Method "PUT" -uri "https://$RubrikCluster/api/internal/vmware/guest_credential/$credentialID" -Headers $head -Body $body
+#================================================
+# Guest Credential Done
+#================================================
+
