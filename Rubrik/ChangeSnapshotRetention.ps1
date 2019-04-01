567731c48f4bf250fed7da3b5ba05d7b20807cc5
Mike Preston <mwpreston@gmail.com>
2019-04-01 13:42:56 -0400

    updated retention

--- Rubrik/ChangeSnapshotRetention.ps1
+++ Rubrik/ChangeSnapshotRetention.ps1
@@ -5,17 +5,20 @@ $cluster = "192.168.150.121"
 $null = Connect-Rubrik -Server $cluster -Credential $creds
 
 #sla domain id to change to
-$sladomainid = (Get-RubrikSLA -Name "Silver").id 
+$sladomainid = (Get-RubrikSLA -Name "Bronze").id 
 
-$unmanaged_vms = (Invoke-RubrikRESTCall -Endpoint "unmanaged_object?object_type=VirtualMachine" -Method "GET" -api "internal").data
+$unmanaged_vms = Get-RubrikUnmanagedObject -Type VirtualMachine
 
 foreach ($vm in $unmanaged_vms)
 {
-    if ($vm.Name -eq 'MPRESTON-VM2')
+    if ($vm.name -eq 'MPRESTON-VM2')
     {       
         $snapshots = (Invoke-RubrikRESTCall -Endpoint "unmanaged_object/$($vm.id)/snapshot" -Method "GET" -api "internal").data.id
-        $body = New-Object -TypeName PSObject -Property @{'slaDomainId'=$sladomainid}
-        $body | Add-Member -Name "snapshotIds" -MemberType NoteProperty -Value $snapshots
+
+        $body = [pscustomobject]@{
+            'slaDomainId'= $sladomainid
+            "snapshotIds" = $snapshots
+        }
         Invoke-RubrikRESTCall -Endpoint "unmanaged_object/snapshot/assign_sla" -Method "POST" -api "internal" -Body $body
     }
 }
