265f29f37ecdc82f1eb068de291d69dd9aabae39
Mike Preston <mwpreston@gmail.com>
2019-09-24 16:13:49 -0400

    changed complete logic

--- Rubrik/GetVMsWithExcludedDisks.ps1
+++ Rubrik/GetVMsWithExcludedDisks.ps1
@@ -1,15 +1,18 @@
-$allVirtualDisks = (Invoke-RubrikRESTCall -Endpoint 'vmware/vm/virtual_disk' -Method GET -api internal).data
-$results = @()
-$total = ($allVirtualDisks | Measure-Object).Count 
+$vms = Get-RubrikVM -PrimaryClusterID local -Relic:$false -DetailedObject
+$totalvms = ($vms | Measure-Object).Count
 $i = 0
-foreach ($virtualdisk in $allVirtualDisks) {
-    $vdInfo = Invoke-RubrikRestCall -Endpoint "vmware/vm/virtual_disk/$($virtualdisk.id)" -Method GET  
+$results = @()
+foreach ($vm in $vms) {
     $i++
-    $percentComplete = ($i/$total)*100
-    Write-Progress -Activity "Searching all virtual disks for exclusions..." -Status "$i of $total"  -PercentComplete $percentComplete
-    if ($vdInfo.excludeFromSnapshots) {
-        $vdInfo | Add-Member -MemberType NoteProperty -Name VMName -Value "$($virtualDIsk.vmName)"
-        $results += $vdInfo
+    $percentComplete = ($i/$totalvms)*100
+    Write-Progress -Activity "Searching all VMs for disk exclusions..." -Status "VM $i of $totalvms"  -PercentComplete $percentComplete
+    foreach ($vdisk in $vm.virtualDiskIds) {
+        $vdInfo = Invoke-RubrikRestCall -Endpoint "vmware/vm/virtual_disk/$vdisk" -Method GET
+        if ($vdInfo.excludeFromSnapshots) {
+            $vdInfo | Add-Member -MemberType NoteProperty -Name VMName -Value "$($vm.Name)"
+            $vdInfo | Add-Member -MemberType NoteProperty -Name VMId -Value "$($vm.id)"
+            $results += $vdInfo
+        }
     }
     
 }
